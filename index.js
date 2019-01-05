63499eb3f8702734bf9f549a1a1654c10d6381bf
Jeremiah  Acayturri <jtacayturri@gmail.com>
2019-01-05 23:48:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function ilovetheBeatles(number) {
 const ilovetheBeatles = [];
 
 do { 
-  ilovetheBeatles.push(`I love the Beatles!`); n++;
+  ilovetheBeatles.push("I love the Beatles!"); n++;
 } while (number < 15); 
 return ilovetheBeatles; 
 }
